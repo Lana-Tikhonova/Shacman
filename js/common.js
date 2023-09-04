faeb040542fa7e3802615f14f1fe6a02fe717627
Svetlana <frontend.lana@gmail.com>
2023-09-04 15:35:46 +0600

    стр услуг, стр товара

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //слайдер на главной
     var swiperWallet = new Swiper(".slider_block .swiper", {
         slidesPerView: "auto",
         spaceBetween: 20,
@@ -24,11 +25,73 @@ $(document).ready(function () {
         $('.mobile_menu').removeClass('active');
     });
 
+    //картинка на весь экран
     $('[data-fancybox="gallery"]').fancybox({
         thumbs: {
             loop: true,
             autoFocus: false,
-        }
+        },
+        mobile: {
+            clickSlide: "close"
+        },
     });
+
+    // слайдер в карточке продукта
+    //маленькие слайды
+    var swiperShopOptionsTwo = {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        grabCursor: true,
+        watchOverflow: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        breakpoints: {
+            769: {
+                spaceBetween: 20,
+            },
+        },
+    };
+
+    let swiperShopTwo = new Swiper(".gallery_small", swiperShopOptionsTwo);
+
+    //большой слайд
+    var swiperShopOptions = {
+        slidesPerView: 1,
+        speed: 500,
+        spaceBetween: 10,
+        grabCursor: true,
+        watchOverflow: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        mousewheelControl: true,
+        thumbs: {
+            swiper: swiperShopTwo,
+        },
+        navigation: {
+            nextEl: '.gallery_big .swiper-button-next',
+            prevEl: '.gallery_big .swiper-button-prev',
+        },
+    };
+
+    let swiperShop = new Swiper(".gallery_big", swiperShopOptions);
+
+    //tabs
+    $('.tabs_wrapper').each(function (e) {
+        let tabsItem = $(this).find('.tabs_content .tabs_content_item');
+        tabsItem.hide().filter(':first').show();
+        $(this).find('.tabs_list .tabs_item').filter(':first').addClass('active')
+    });
+
+    // Клики по вкладкам
+    $('.tabs_item').click(function (e) {
+        e.preventDefault();
+        let tabsItem = $(this).parents('.tabs_wrapper').find('.tabs_content .tabs_content_item');
+        tabsItem.hide();
+        tabsItem.filter(this.hash).show();
+        $(this).parents('.tabs_wrapper').find('.tabs_list .tabs_item').removeClass('active');
+        $(this).addClass('active');
+        return false;
+    });
+
 
 });
