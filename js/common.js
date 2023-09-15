fe29fc3e6e9f360c129338103a5d8f7e97641c57
Svetlana <frontend.lana@gmail.com>
2023-09-15 16:06:44 +0600

    modal

--- js/common.js
+++ js/common.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
 
     // слайдер в карточке продукта
     //маленькие слайды
-    var swiperShopOptionsTwo = {
+    var swiperShopOptionsSmall = {
         slidesPerView: 3,
         spaceBetween: 10,
         grabCursor: true,
@@ -77,7 +77,7 @@ $(document).ready(function () {
         },
     };
 
-    let swiperShopTwo = new Swiper(".gallery_small", swiperShopOptionsTwo);
+    let swiperShopSmall = new Swiper(".gallery_small_shop", swiperShopOptionsSmall);
 
     //большой слайд
     var swiperShopOptions = {
@@ -90,15 +90,16 @@ $(document).ready(function () {
         watchSlidesProgress: true,
         mousewheelControl: true,
         thumbs: {
-            swiper: swiperShopTwo,
+            swiper: swiperShopSmall,
         },
         navigation: {
-            nextEl: '.gallery_big .swiper-button-next',
-            prevEl: '.gallery_big .swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
     };
 
-    let swiperShop = new Swiper(".gallery_big", swiperShopOptions);
+    let swiperShop = new Swiper(".gallery_big_shop", swiperShopOptions);
+
 
     //tabs
     $('.tabs_wrapper').each(function (e) {
@@ -119,12 +120,14 @@ $(document).ready(function () {
         return false;
     });
 
+    // таблицы подстраиваются друг под друга
     var $grid = $('.tabs_content_table').masonry({
         // options
         itemSelector: 'table',
         // columnWidth: 900
     });
 
+    // фикс меню 
     function fixMenu(e) {
         if ($(this).scrollTop() > 10) {
             $('.mobile_menu_close').addClass('fix');
@@ -141,6 +144,7 @@ $(document).ready(function () {
         fixMenu();
     });
 
+    // маскадля телефона
     const phoneInputs = document.querySelectorAll('.form_input[name="tel"]');
     phoneInputs.forEach(input => {
         IMask(input, {
@@ -148,6 +152,7 @@ $(document).ready(function () {
         })
     })
 
+    // валидация
     function removeErr(e) {
         e.target.closest('.form_input_group').classList.remove('error');
     }
@@ -173,7 +178,99 @@ $(document).ready(function () {
         });
     });
 
+    // звездочки в отзывах
+    $('.review_rating span').hover(
+        function () {
+            var n = $(this).data('n');
+            for (var i = 1; i <= n; i++) {
+                $('.review_rating span[data-n="' + i + '"]').addClass('hover');
+            }
+        },
+        function () {
+            $('.review_rating span').removeClass('hover');
+        }
+    );
+    $('.review_rating span').click(function () {
+        $('.review_rating span').removeClass('active');
+        var n = $(this).data('n');
+        for (var i = 1; i <= n; i++) {
+            $('.review_rating span[data-n="' + i + '"]').addClass('active');
+        }
+        $('.review_rating input').val(n);
+    });
+
+
+    // открытие модаки 
+    // можно использовать и для других модалок, не только для быстрого просмотра
+    // нужно только поменять значени в data-modal и data-open-modal
+    const body = document.querySelector('body');
+    let getScrollWidth = () => window.innerWidth - document.documentElement.offsetWidth;
+    let browserScrollWidth = getScrollWidth();
+
+    document.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.closest('[data-open-modal]')) {
+            e.preventDefault();
+            const targetId = target.closest('[data-open-modal]').dataset.openModal;
+            const selectedModal = document.querySelector(`[data-modal="${targetId}"]`);
+            selectedModal.classList.add('show');
+            body.classList.add('locked');
+            if (getScrollWidth() == 0) {
+                body.style.paddingRight = `${browserScrollWidth}px`;
+            }
+            // инитю слайдер при открытии модалки
+            // слайдер в быстром просмотре
+            //маленькие слайды
+            var swiperViewingOptionsSmall = {
+                slidesPerView: 3,
+                spaceBetween: 10,
+                grabCursor: true,
+                watchOverflow: true,
+                watchSlidesVisibility: true,
+                watchSlidesProgress: true,
+                breakpoints: {
+                    769: {
+                        spaceBetween: 20,
+                    },
+                },
+            };
+
+            let swiperViewingSmall = new Swiper(".gallery_small_viewing", swiperViewingOptionsSmall);
+
+            //большой слайд
+            var swiperViewingOptions = {
+                slidesPerView: 1,
+                speed: 500,
+                spaceBetween: 10,
+                grabCursor: true,
+                watchOverflow: true,
+                watchSlidesVisibility: true,
+                watchSlidesProgress: true,
+                mousewheelControl: true,
+                thumbs: {
+                    swiper: swiperViewingSmall,
+                },
+            };
+
+            let swiperViewing = new Swiper(".gallery_big_viewing", swiperViewingOptions);
+        }
+        if (target.closest('[data-modal-close]')) {
+            e.preventDefault();
+            let modalOpen = document.querySelector('.modal.show');
+            modalOpen.classList.remove('show');
+            body.classList.remove('locked');
+            body.style.paddingRight = ``;
+        }
+        if (target.closest('.modal') && !target.closest('.modal-content')) {
+            e.preventDefault();
+            let modalOpen = document.querySelector('.modal.show');
+            modalOpen.classList.remove('show');
+            body.classList.remove('locked');
+            body.style.paddingRight = ``;
+        }
+    });
 });
+// калькулятор
 window.onload = function () {
     if (document.getElementsByClassName("home_calc").length) {
 
